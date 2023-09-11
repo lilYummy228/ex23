0ae5cef3bfa8118deee1f26dfa539ea6abf1b35d
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2023-09-11 12:31:20 +0500

    Completed

--- ex23/Program.cs
+++ ex23/Program.cs
@@ -10,7 +10,38 @@ namespace ex23
     {
         static void Main(string[] args)
         {
+            int[] array = new int[10];
+            int minRandomValue = 0;
+            int maxRandomValue = 10;
+            int tempNumber;
+            Random random = new Random();
+            Console.WriteLine("Обычный массив: ");
 
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(minRandomValue, maxRandomValue);
+                Console.Write($"{array[i]}|");
+            }
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        tempNumber = array[i];
+                        array[i] = array[j];
+                        array[j] = tempNumber;
+                    }
+                }
+            }
+
+            Console.WriteLine("\nОтсортированный массив: ");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]}|");
+            }
         }
     }
 }
